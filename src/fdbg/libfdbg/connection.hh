7b4abddd79f9ca6296b8b95375709a3bb92e05d4
Andre Wagner <WagnerAndre@JohnDeere.com>
2024-04-28 14:58:23 -0500

    WIP

--- src/fdbg/libfdbg/connection.hh
+++ src/fdbg/libfdbg/connection.hh
@@ -6,6 +6,8 @@
 #include "request.pb.h"
 #include "response.pb.h"
 
+enum class DebuggingLevel { NORMAL, DEBUG, TRACE };
+
 class Connection {
 public:
     Connection(const char* port, uint32_t baudrate);
@@ -14,8 +16,11 @@ public:
     void     send_request(Request const& request) const;
     Response receive_response() const;
 
+    void set_debugging_level(DebuggingLevel debugging_level) { debugging_level_ = debugging_level; }
+
 private:
-    int master_fd;
+    int master_fd_ = -1;
+    DebuggingLevel debugging_level_ = DebuggingLevel::NORMAL;
 };
 
 #endif
