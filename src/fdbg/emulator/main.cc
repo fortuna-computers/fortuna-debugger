0dd060cb3e5ec68937e8d9d6336eb325b1684b7c
Andre Wagner <WagnerAndre@JohnDeere.com>
2024-04-29 11:48:05 -0500

    WIP

--- src/fdbg/emulator/main.cc
+++ src/fdbg/emulator/main.cc
@@ -2,31 +2,33 @@
 
 #include <cstdio>
 
+extern "C" {
+#include "../machine.h"
+}
+
 #include <thread>
 using namespace std::chrono_literals;
 
 int main()
 {
-    uint8_t ram[64 * 1024];
-
     fdbg::ComputerServer server;
     server.set_debugging_level(fdbg::DebuggingLevel::TRACE);
-
-    printf("%s\n", server.port().c_str());
 
     for (;;) {
         auto omsg = server.receive();
 
         if (omsg) {
             if (omsg->has_ack()) {
-                server.send_ack_response(0x1234);
+                server.send_ack_response(machine_characteristics()->id);
             }
 
+            /*
             if (omsg->has_write_memory()) {
                 WriteMemory const& wm = omsg->write_memory();
                 for (uint64_t i = 0; i < wm.bytes().size(); ++i)
                     ram[wm.initial_addr() + i] = wm.bytes().at(i);
             }
+             */
         }
 
         std::this_thread::sleep_for(1ms);
