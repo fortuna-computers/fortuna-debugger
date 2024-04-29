4754544f63ac40dc8dc11850291b6a9230267e70
Andre Wagner <WagnerAndre@JohnDeere.com>
2024-04-29 14:05:25 -0500

    WIP

--- src/fdbg/debugger/main.cc
+++ src/fdbg/debugger/main.cc
@@ -18,6 +18,69 @@ int main()
         throw std::runtime_error("Error " + std::to_string(error) + ": " + description);
     });
 
+    // window hints
+    glfwWindowHint(GLFW_DOUBLEBUFFER, 1);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_DEPTH_BITS, 24);
+    glfwWindowHint(GLFW_STENCIL_BITS, 8);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    std::string glsl_version = "#version 150";
+
+    // create window
+    GLFWwindow* window = glfwCreateWindow(1200, 800, "Fortuna debugger", nullptr, nullptr);
+
+    // setup OpenGL
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGL(glfwGetProcAddress))
+        throw std::runtime_error("Could not initialize GLAD.");
+    glClearColor(0, 0, 0, 1.0f);
+
+    // configure GLFW
+    glfwSwapInterval(1);
+    glfwSetFramebufferSizeCallback(window, [](GLFWwindow*, int width, int height) { glViewport(0, 0, width, height); });
+
+    // configure ImGui
+    IMGUI_CHECKVERSION();
+    ImGuiContext* context = ImGui::CreateContext();
+    ImGuiIO* io_ = &ImGui::GetIO();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version.c_str());
+    ImGui::StyleColorsDark();
+    io_->KeyRepeatRate = 0.1f;
+
+    // main loop
+    while (!glfwWindowShouldClose(window)) {
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // TODO - draw
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+
+        // TODO - check for keypresses
+
+        // TODO - yield
+    }
+
+    // terminate
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    if (window)
+        glfwDestroyWindow(window);
+    glfwTerminate();
+
     /*
     fdbg::DebuggerClient client("/dev/ttys001");
     client.set_debugging_level(fdbg::DebuggingLevel::TRACE);
