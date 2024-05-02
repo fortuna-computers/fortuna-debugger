0a6d5bb6e554987e9ce944e883b2837f8e7c40c4
Andre Wagner <WagnerAndre@JohnDeere.com>
2024-05-01 20:11:24 -0500

    WIP

--- sample-chip8/microcontroller.c
+++ sample-chip8/microcontroller.c
@@ -1,7 +1,11 @@
 #include "microcontroller.h"
 
+#include <stdio.h>
+#include "pico/stdlib.h"
+
 void microcontroller_init()
 {
+    stdio_init_all();
 }
 
 bool uart_ready()
@@ -11,9 +15,10 @@ bool uart_ready()
 
 void uart_send_byte(uint8_t byte)
 {
+    putchar(byte);
 }
 
 uint8_t uart_receive_byte()
 {
-    return 0;
+    return getchar();
 }
